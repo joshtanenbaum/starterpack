7e66e359db16ca9c443f9ea65778f96062e7ade2
Josh Tanenbaum <Joshua.D.Tanenbaum.20@dartmouth.edu>
2020-04-28 11:57:53 -0400

    sa5 done

--- src/index.js
+++ src/index.js
@@ -1,18 +1,59 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
 // change require to es6 import style
-import $ from 'jquery';
 import './style.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router, Route, NavLink, Switch,
+} from 'react-router-dom';
 
-function p(x) {
-  const num = (new Date().getTime()) - x;
-  $('#main').html(`Youve been on this page for ${num / 1000} seconds.`);
-}
-
-const starttime = new Date().getTime();
-setInterval(() => { p(starttime); }, 1000);
+const About = (props) => {
+  return <div> All there is to know about me </div>;
+};
 
 
-const App = () => <div className="test">All the REACT are belong to us!</div>;
+const Welcome = (props) => {
+  return <div>Welcome</div>;
+};
+
+
+const Test = (props) => {
+  return <div> ID: {props.match.params.id} </div>;
+};
+
+
+const FallBack = (props) => {
+  return <div>URL Not Found</div>;
+};
+
+const App = (props) => {
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Welcome} />      {/* set components on page for a given url, components get some props automatically */}
+          <Route path="/about" component={About} />
+          <Route exact path="/test/:id" component={Test} />
+          <Route component={FallBack} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
+
+
+const Nav = (props) => {
+  return (
+    <nav>
+      <ul>
+        <li><NavLink to="/" exact>Home</NavLink></li>      {/* take us to new url */}
+        <li><NavLink to="/about">About</NavLink></li>   {/* take us to new url */}
+        <li><NavLink to="/test/id1">test id1</NavLink></li>
+        <li><NavLink to="/test/id2">test id2</NavLink></li>
+      </ul>
+    </nav>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById('main'));
